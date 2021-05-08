cd6948a70b08b71ef174c4aeb164bbee812ce1f7
roku.azumi <7visionary113@gmail.com>
2021-05-08 03:12:54 +0000

    user login

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 
+@if (Auth::check())
  <h1>タスク一覧</h1>
 
     @if (count($tasks) > 0)
@@ -26,4 +27,12 @@
     @endif
 
     {!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'btn btn-primary']) !!}
+@else
+ <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the Tasklist</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+        </div>
+    </div>
+@endif
 @endsection
